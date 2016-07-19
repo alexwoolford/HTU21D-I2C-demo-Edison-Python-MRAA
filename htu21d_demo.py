48b72044237f19bf778e901d45cefe8af5dd051b
Alexander Woolford <awoolford@HW13455.woolford.io>
2016-07-18 22:34:12 -0600

    Continuous read

--- htu21d_demo.py
+++ htu21d_demo.py
@@ -6,14 +6,10 @@ from htu21d import HTU21D
 # Initialise the HTU21D
 htu = HTU21D(1, False)    # add <True> to enable all debug messages
 
-# Optional <True> debug enable may be sent to each HTU21D command
-print "User Reg1: {}".format(hex(htu.readUserReg()))
-
-print "Change HTU1 resolution"
 htu.writeUserReg(0xc3)
 print "User Reg: {}".format(hex(htu.readUserReg()))
 
-for _ in range(20):
+while True:
 
     temp = htu.readTemperatureData()
     rh = htu.readRHData()
