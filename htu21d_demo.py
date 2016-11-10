ecfa1404e84bfa3d237f9d23e951843f8d37f7b8
Alex Woolford <alex@woolford.io>
2016-11-10 10:17:06 -0700

    logs temperature as JSON to Kafka

--- htu21d_demo.py
+++ htu21d_demo.py
@@ -1,19 +1,44 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-import time
 from htu21d import HTU21D
+import socket
+import time
+import json
+from kafka import KafkaProducer
 
-# Initialise the HTU21D
-htu = HTU21D(1, False)    # add <True> to enable all debug messages
 
-htu.writeUserReg(0xc3)
-print "User Reg: {}".format(hex(htu.readUserReg()))
+class TemperatureKafkaLogger:
 
-while True:
+    """
+        Log temperature, as JSON, to a Kafka topic.
+    """
 
-    temp = htu.readTemperatureData()
-    rh = htu.readRHData()
+    def __init__(self):
+        # Initialise the HTU21D
+        self.htu = HTU21D(1, False)    # add <True> to enable all debug messages
 
-    print "Temperature: %.2f C, RH: %.2f %%" % (temp, rh)
+        self.htu.writeUserReg(0xc3)
+        # print "User Reg: {}".format(hex(self.htu.readUserReg()))
 
-    time.sleep(0.1)
+        self.host = socket.gethostname()
+
+        self.producer = KafkaProducer(bootstrap_servers='hadoop02:6667')
+
+    def run(self):
+        while True:
+
+            # capture temp, relative humidity, and timestamp
+            celcius = self.htu.readTemperatureData()
+            rh = self.htu.readRHData()
+            epoch_time = time.time()
+
+            record = {'host': self.host, 'ts': epoch_time, 'celcius': celcius, 'rh': rh}
+
+            # write to Kafka topic
+            self.producer.send('temperature', json.dumps(record))
+
+            time.sleep(0.1)
+
+if __name__ == "__main__":
+    temperature_kafka_logger = TemperatureKafkaLogger()
+    temperature_kafka_logger.run()
